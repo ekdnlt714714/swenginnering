5fefdb35eccd3fdcbf9a53666e0d12f4380656d3
Euichan Park <116147712+ekdnlt714714@users.noreply.github.com>
2025-05-20 11:45:24 +0900

    master1

---
+++ StudentManager/src/student/StudentManagerTest2.java
@@ -0,0 +1,45 @@
+package student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StudentManagerTest {
+   private StudentManager studentManager;
+   
+   @BeforeEach
+   void setUp() throws Exception {
+      studentManager = new StudentManager();
+   }
+
+   @Test
+   void testAddStudent() {
+      studentManager.addStudent("박의찬");
+      assertTrue(studentManager.hasStudent("박의찬"), "학생 추가");
+   }
+
+   @Test
+   void testRemoveStudent() {
+       studentManager.addStudent("의찬");
+       studentManager.removeStudent("의찬");
+       assertFalse(studentManager.hasStudent("의찬"), "학생 제거");
+   }
+
+   @Test
+   void testAddStudentDuplicate() {
+       studentManager.addStudent("의찬박");
+       assertThrows(IllegalArgumentException.class, () -> {
+           studentManager.addStudent("의찬박");
+       }, "예외 발생");
+   }
+
+   @Test
+   void testRemoveNonExistentStudent() {
+       assertThrows(IllegalArgumentException.class, () -> {
+           studentManager.removeStudent("박박");
+       }, "예외 발생");
+   }
+
+
+}
